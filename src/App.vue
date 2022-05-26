6b170157f6c9c99cb8d91fd739780c05f88b1e93
陈文真 <chenwenzhen@chenwenzhendeMacBook-Pro.local>
2022-05-26 11:34:07 +0800

    feat: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img
+      class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none"
+      src="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
+      alt=""
+    />
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        Getting a new business off the ground is a lot of hard work. Here are
+        five ideas you can use to find your first customers.
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">cwzp990</div>
+        <div class="text-slate-500">这里是副标题</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup></script>
